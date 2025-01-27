80877e3df88d0fd5e91a92eec94af690ac21fab2
isim soyisim <bahadir_kestane@hotmail.com>
2025-01-27 15:39:22 +0300

    Category Controller eklendi

---
+++ Muhasebe/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Muhasebe.BusinessLayer.Concrete;
+using Muhasebe.DataAccessLayer.EntityFramework;
+
+namespace Muhasebe.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager _categoryManager = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index()
+        {
+            var values=_categoryManager.GetAll();
+
+            return View(values);
+        }
+    }
+}
